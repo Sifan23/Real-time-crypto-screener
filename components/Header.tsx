9fedaa3c52f7bbf7a38f9369421186e76a91533d
Sifan23 <sifanyg123@gmail.com>
2026-01-12 20:40:04 +0300

    refactoring

--- components/Header.tsx
+++ components/Header.tsx
@@ -18,10 +18,10 @@ const Header = () => {
           height={40}
         />
       </Link>
-      <nav>
+      <nav aria-label="Main navigation">
         <Link href="/" className={cn('nav-link', {'is-active': pathname === '/', 'is-home': true})}>Home</Link>
         <p>Search Modal</p>
-        <Link href='/coins' className={cn('nav-link', {"is-active": pathname === "/coins"})}>All Coins</Link>
+        <Link href='/coins' className={cn('nav-link', {"is-active": pathname.startsWith("/coins")})}>All Coins</Link>
       </nav>
     </div>
     </header>
