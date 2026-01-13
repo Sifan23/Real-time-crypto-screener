e3819615f4ac2f73d0ba4808acc75a559519afb0
Sifan23 <sifanyg123@gmail.com>
2026-01-13 20:19:36 +0300

    feat: coinoverview, trending coins, categories sections starter

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import DataTable from "@/components/DataTable";
+import { Section } from "lucide-react";
+import Image from "next/image";
+import React from "react";
 
 const Page = () => {
   return (
-    <></>
-  )
-}
+    <main className="main-container">
+      <section className="home-grid">
+        <div id="coin-overview">
+          <div className="header pt-2">
+            <Image
+              src="https://assets.coingecko.com/coins/images/1/large/bitcoin.png"
+              alt="Bitcoin logo"
+              width={100}
+              height={100}
+            />
+            <div className="info">
+                <p>Bitcoin / BTC</p>
+                <p>$89,113.00</p>
+            </div>
+          </div>
+        </div>
+        <p>Trending Coins</p>
+        {/* <DataTable/> */}
+      </section>
 
-export default Page
+      <section className="w-full mt-7 space-y-4">
+        <p>Categories</p>
+      </section>
+    </main>
+  );
+};
+
+export default Page;
